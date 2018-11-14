c1898c88ee2177dc1a2c94756114d68c96a22795
Daniel Jeffries <danjeffries96@gmail.com>
2018-11-14 18:27:14 -0500

    no changes

--- canvas-util.js
+++ canvas-util.js
@@ -255,7 +255,7 @@ class CanvasState {
         var active = 
           (this.activeObj && 
             this.activeObj.getParent() === obj ) || this.selectGroup.has(obj);
-        obj.draw(active);
+          obj.draw(active);
     });
 
   }
