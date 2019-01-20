ecb421638924ba729016400b89c2c3985284a4fd
danjeffries96 <danjeffries96@github.com>
2019-01-20 01:52:04 -0500

    server now expects setClipId then a blob, since client assigns ids now

--- server-connection.js
+++ server-connection.js
@@ -1,5 +1,3 @@
-const vm = require("./video").VideoManager;
-
 function urlToPath(url) {
   var urlPattern = /temp\/\d+\/\d+\.[a-zA-Z\d]+$/; 
   var match = url.match(urlPattern);
@@ -7,21 +5,36 @@ function urlToPath(url) {
 };
 
 function processClientMessage(ws, req, message) {
-  console.log("message from client: ", message);
+  // this client's VideoManager = req.session.vm
+  var vm = req.session.vm;
+
+  console.log("new message:", message);
   // handle blob -- end of recording
   if (message instanceof Buffer) {
-    vm.addClip(ws, req.session.id, message);
+    // client must also send id of clip being added
+    if (req.session.currentClipId == null)
+      throw "Cannot add clip; id hasn't been sent";
+    vm.addClip(req.session.currentClipId, message);
+
+    req.session.currentClipId = null;
     return;
   }
 
   var msgObj = JSON.parse(message);
   var body = msgObj.body;
-                                                          
+                                                         
   switch (msgObj.type) {
-    case "truncate":
-      var filePath = urlToPath(body.url);
-      vm.truncateClip(ws, filePath, body.timeStamp);
+    case "setClipId":
+      req.session.currentClipId = msgObj.body.id; 
       break;
+    case "truncate":
+      vm.truncateClip(body.clipId, body.timeStamp);
+      break;
+    case "select":
+      vm.selectClip(body.clipId);
+      break;
+    case "merge":
+      vm.mergeClips(body.clipIds);
   }
 };
 
