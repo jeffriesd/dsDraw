ff722282a2100e71d1a88d0da1039ae9cb54bfa6
danjeffries96 <danjeffries96@github.com>
2019-01-20 01:55:02 -0500

    remodel truncate and export (merge). add blank/cont for testing

--- public/util-command.js
+++ public/util-command.js
@@ -62,14 +62,14 @@ class StopCommand extends VideoCommand {
 class TruncateVideoCommand extends VideoCommand {
   // truncate video file as well as command recorder state
   execute() {
+    if (this.mc.getState() !== this.mc.pauseState) return;
     var conn = WebSocketConnection.getInstance();
-    var mc = MediaController.getInstance();
-    var currTime = mc.player.video.currentTime;
+    var currTime = this.mc.player.video.currentTime;
 
-    var clipId = mc.activeClipId;
+    var clipId = this.mc.activeClipId;
 
-    mc.waiting = true;
-    mc.cmdRecorder.truncate(currTime);
+    this.mc.waiting = true;
+    this.mc.cmdRecorder.truncate(currTime);
 
     // send message from client to server to truncate video at current time
     var body = { clipId : clipId, timeStamp: currTime };
@@ -88,15 +88,95 @@ class SelectVideoCommand extends VideoCommand {
    *    and switch command recorder to correct one
    */
   execute() {
+    if (this.mc.getState() !== this.mc.pauseState) return;
     var conn = WebSocketConnection.getInstance();
-   
-    var mc = MediaController.getInstance();
-    // before changing clips, clear canvas
-    mc.cmdRecorder.seekTo(-1);
-
-    mc.activeClipId = this.clipId;
 
     var body = { clipId: this.clipId};
     conn.sendServer("select", body);
   }
 }
+
+class ExportVideoCommand extends VideoCommand {
+  constructor(cState, ...clipIds) {
+    super(cState);
+
+    // looks clunky but parseInt takes up to 3 args,
+    // so regular map(parseInt) doesn't work
+    this.clipIds = clipIds.map(x => parseInt(x)); 
+  }
+
+  /** ExportVideoCommand.execute
+   *   merge clips together and prompt user
+   *   to download
+   */
+  execute() {
+    if (this.mc.getState() !== this.mc.pauseState) return;
+
+    var conn = WebSocketConnection.getInstance();
+
+    // TODO set merging state -- disable clip menu and controls
+    this.mc.waiting = true;
+    
+    // // merge commandRecorders by tracking elapsed time
+    // // and offsetting subsequent clips by this amount
+    // var mergedRec = new CommandRecorder(this.mc.player, this.mc.framerate);
+    // var seconds = 0;
+    // this.clipIds.forEach(clipId => {
+    //   this.mc.activeClipId = clipId; // cmdRecorder getter uses activeClipID
+    //   var duration = this.mc.durations.get(clipId);
+
+    //   // use temporary array since clip i+1 commands
+    //   // need to come before clip i commands in stack
+    //   var futureCmds = [];
+    //   
+    //   // clear contents and make sure
+    //   // all commands are in one stack
+    //   this.mc.cmdRecorder.seekTo(-1);
+
+    //   this.mc.cmdRecorder.futureCmds.forEach(ct => {
+    //     var cmdTime = Object.assign(ct);
+    //     cmdTime.time += seconds;
+    //     console.log("new command at ", cmdTime.time);
+    //     futureCmds.push(cmdTime)
+    //   });
+
+    //   // cmds = [clip i, clip i-1, ... , clip 0]
+    //   // cmds = [clip i+1, clip i, ... , clip 0]
+    //   mergedRec.futureCmds = futureCmds.concat(mergedRec.futureCmds);
+  
+    //   // increment time by clip duration
+    //   seconds += duration;
+    //   console.log("offset time = ", seconds);
+
+    //   // discard this command recorder from MC map
+    //   this.mc.removeClip(clipId);
+    // });
+
+    // mergedRec.printStacks();
+
+    // // store merged command recorder for retrieval
+    // // by MediaController once server does merging and 
+    // // assigns new clip id
+    // this.mc.commandRecorders.set(-1, mergedRec);
+
+    var body = { clipIds: this.clipIds };
+    conn.sendServer("merge", body);
+  }
+}
+
+class ContinueClipCommand extends VideoCommand {
+  
+  /** ContinueClipCommand.execute
+   *    create new clip (new thumbnail and id)
+   */
+  execute() {
+    this.mc.newClipFromCurrent();
+  }
+}
+
+class BlankClipCommand extends VideoCommand {
+  execute() {
+    this.mc.newClipBlank();
+  }
+}
+
