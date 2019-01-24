5c4ebb3722a1d7d2ed3e15bc3ed9999480152827
danjeffries96 <danjeffries96@github.com>
2019-01-24 18:58:06 -0500

    object equivalence and map key equiv prototypes

--- public/constants.js
+++ public/constants.js
@@ -2,20 +2,69 @@ Array.prototype.peek = function() {
   return this.length ? this[this.length - 1] : null;
 };
 
-SHIFT = 16;
-CTRL = 17;
-ALT = 18;
-ENTER = 13;
-ESC = 27;
-UP = 38;
-DOWN = 40;
+// object equivalence
+Object.prototype.equiv = function(other) {
+  var thisProps = Object.getOwnPropertyNames(this);
+  var otherProps = Object.getOwnPropertyNames(other);
 
-C = 67;
-L = 76;
-Y = 89;
-Z = 90;
+  if (thisProps.length != otherProps.length) return false;
 
-PLAYBTN = "url(../images/play.png)";
-PAUSEBTN = "url(../images/pause.png)";
+  for (let prop in this) {
+    thisType = typeof this[prop];
+    otherType = typeof other[prop];
 
-DEFAULT_THUMBNAIL = "images/default_thumb.png";
+    if (thisType !== otherType) return false;
+
+    if (thisType == "object") {
+      // recursive case for objects
+      if (! this[prop].equiv(other[prop])) return false;
+    }
+    else if (this[prop] !== other[prop])
+      return false;
+  }
+  return true;
+};
+
+// wrapper for generic a, b (not necc object)
+function equivalent(a, b) {
+  if (typeof a != typeof b) return false;
+  if (typeof a == "object") return a.equiv(b);
+  return a === b;
+}
+
+Map.prototype.hasEquiv = function(key) {
+  return Array.from(this.keys()).some(k => equivalent(k, key));
+}
+
+const mget = Map.prototype.get;
+// allow array key equivalence
+Map.prototype.get = function(key) {
+  if (mget.bind(this)(key)) return mget.bind(this)(key);
+  var match;
+  this.forEach((value, k) => {
+    if (equivalent(k, key)) match = value;
+  });
+  return match;
+}
+
+
+const SHIFT = 16;
+const CTRL = 17;
+const ALT = 18;
+const ENTER = 13;
+const ESC = 27;
+const UP = 38;
+const DOWN = 40;
+
+const C = 67;
+const L = 76;
+const Y = 89;
+const Z = 90;
+
+const PLAYBTN = "url(../images/play.png)";
+const PAUSEBTN = "url(../images/pause.png)";
+      
+const DEFAULT_THUMBNAIL = "images/default_thumb.png";
+      
+// text height for flowchart text
+const TEXT_HEIGHT = 14;
