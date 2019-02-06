46f2638ef52e61c699dd38f6e981c2c8f8696fb9
danjeffries96 <danjeffries96@github.com>
2019-02-06 13:42:41 -0500

    trying out single char default labels

--- public/canvas-object.js
+++ public/canvas-object.js
@@ -1,5 +1,3 @@
-_
-
 /** CanvasObject
  *    class for macro objects such as textbox, arrows,
  *    (not ResizePoint or ArrowHead, etc.)
@@ -24,10 +22,10 @@ class CanvasObject {
     this._label = "";
 
     // default label: convert 'rgb(1, 2, 3)' to 123
-    var hashStr = this.hashColor.split("rgb(")[1];
-    hashStr = hashStr.split(")")[0];
-    var hashCode = hashStr.replace(/[,\s]/g, "");
-    this.label = this.constructor.name + "_" + hashCode;
+    // var hashStr = this.hashColor.split("rgb(")[1];
+    // hashStr = hashStr.split(")")[0];
+    // var hashCode = hashStr.replace(/[,\s]/g, "");
+    this.label = this.constructor.name.substring(0,1).toLowerCase(); // + "_" + hashCode;
   }
 
   clone() {
@@ -42,23 +40,18 @@ class CanvasObject {
   }
 
   set label(value) {
-    if (value == "" || value == "TEMP")
+    if (value == "TEMP")
       return;
 
-    // object label must begin with 
-    // alphabetic character
-    var alpha = /^[a-zA-Z]+/
-    if (! value.match(alpha))
-      throw "Label must begin with alphabetic character";
-    var alphaNum = /^[a-zA-Z0-9_]*/;
-    if (! value.match(alphaNum))
-      throw "Label may consist only of alphanumeric characters and underscores";
+    if (! value.match(variablePattern))
+      throw "Variable name must consists of only alphanumeric (or _) characters" 
+        + " and begin with an alphabetic character";
 
     // if 'myname1' already exists, 
     // set to 'myname2' unless user 
     // tries to rename object by its current name (don't change it)
-    while (this.cState.labeled.get(value)) {
-      if (this.cState.labeled.get(value) === this) break;
+    while (VariableEnvironment.hasVar(value)) {
+      if (VariableEnvironment.getVar(value) === this) break;
       console.log("[WARNING]: label already in use: ", value);
       var numPattern = /\d*$/;
       var match = value.match(numPattern);
@@ -74,12 +67,12 @@ class CanvasObject {
 
     // if previous name is already mapping to this object, 
     // clear that mapping
-    if (this.cState.labeled.get(this.label)) {
-      this.cState.labeled.delete(this.label);
+    if (VariableEnvironment.hasVar(this.label)) {
+      VariableEnvironment.deleteVar(this.label);
       // console.log("clearing label for ", this.label);
     }
     
-    this.cState.labeled.set(value, this); 
+    VariableEnvironment.setVar(value, this);
 
     this._label = value;
   }
@@ -123,6 +116,7 @@ class CanvasObject {
 
   restore() {
     this.cState.addCanvasObj(this);
+    VariableEnvironment.setVar(this.label, this);
   }
 
   deactivate() {
@@ -183,6 +177,14 @@ class CanvasObject {
   release() {
   }
 
+  static outline(ctx, x1, y1, x2, y2) {
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#000";
+    ctx.beginPath();
+    ctx.rect(x1, y1, x2-x1, y2-y1);
+    ctx.stroke();
+  }
+
 }
 
 class CanvasChildObject {
@@ -226,6 +228,11 @@ class CanvasChildObject {
   }
 
   release() {
+  }
+
+  restore() {
+    this.cState.registerCanvasObj(this);
+    VariableEnvironment.setVar(this.label, this);
   }
 
   /** CanvasChildObject.hover
