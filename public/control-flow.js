326560ff4fa560c4d85e20189f5a028b779f99bc
danjeffries96 <danjeffries96@github.com>
2019-02-13 00:04:23 -0500

    command objects for control flow blocks

---
+++ public/control-flow.js
@@ -0,0 +1,105 @@
+// TODO
+// decorator pattern (maybe) to highlight lines
+// debugger style 
+
+// if (cond1) {
+//
+// } else (cond2) {
+//  
+// } [else (cond n) {} ]
+// [else {
+//
+// }]
+//
+//
+//  for (;;) {
+//    s1;
+//    s2;
+//    s2;
+//  }
+//
+//  after parsing block:
+//    put each statement on its own line
+//
+//    bind statements to line numbers 
+//    at compile time
+//
+//    when blocks execute a line of code,
+//    highlight that line
+//
+//    Loops/blocks could be iterators
+
+const LOOP_MAX = 1000000; // stop infinite loops from 
+                            // taking up too many resources
+
+/** WhileLoopCommand
+ *    accepts Condition function and executes
+ *    each command in a loop as long as
+ *    the condition is satisifed
+ *
+ *    TODO: stop infinite loops-- limit to 1M steps maybe
+ */
+class WhileLoopCommand {
+  constructor(condition, loopStatements) {
+    this.condition = condition;
+    this.loopStatements = loopStatements;
+    this.steps = 0;
+  }
+
+  execute() {
+    while (this.condition.command.execute()) {
+      this.loopStatements.forEach(s => s.command.execute());
+      if (this.steps++ > LOOP_MAX) {
+        alert("Loop is taking too long");
+        break;
+      }
+    }
+  }
+
+  step() {
+    // this.statements.next().execute();
+  }
+
+  undo() {
+
+  }
+}
+
+/** ForLoopCommand
+ *  
+ *  syntax:
+ *  
+ *  for ((expr|"");(condition);(expr|"") {
+ *    (expr1)
+ *    `command1`
+ *    (expr2)
+ *    `command2`
+ *  }
+ */
+class ForLoopCommand {
+  constructor(initStatements, condition, incrStatements, loopStatements) {
+    this.initStatements = initStatements;
+    this.condition = condition;
+    this.incrStatements = incrStatements;
+    this.loopStatements = loopStatements;
+    console.log("loop statements = ", this.loopStatements);
+    this.steps = 0;
+  }
+
+  execute() {
+    this.initStatements.forEach(s => s.command.execute());
+    while (this.condition == null || this.condition.command.execute()) {
+      this.loopStatements.forEach(s => { 
+        s.command.execute();
+        console.log("executing:", s.constructor.name);
+      });
+      this.incrStatements.forEach(s => s.command.execute());
+      if (this.steps++ > LOOP_MAX) {
+        alert("Loop is taking too long");
+        break;
+      }
+    }
+  }
+
+}
+
