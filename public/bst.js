185f09c9569034b781628806b57d7ab735012b5e
danjeffries96 <danjeffries96@github.com>
2019-02-17 20:21:40 -0500

    remove value from bst node propNames

--- public/bst.js
+++ public/bst.js
@@ -12,9 +12,9 @@ class BST extends LinearCanvasObject {
     super(canvasState, x1, y1, x2, y2);
     this.root = new BSTNode(canvasState, this, null, 0);
 
-    this.cellSize = 10;
+    this.cellSize = 20;
 
-    for (var i =0; i < 4; i++)
+    for (var i =0; i < 12; i++)
       this.insert(Math.random() * 100 | 0);
 
     this.minSep = 6;
@@ -40,6 +40,18 @@ class BST extends LinearCanvasObject {
     };
   }
 
+  /** BST.getChildren
+   *    return nodes with values 
+   *    in the range
+   */
+  getChildren(low, high) {
+    if (low == null) low = this.root.getMin().value;
+    if (high == null) high = this.root.getMax().value;
+    
+    return this.inorder()
+      .filter(node => node.value >= low && node.value <= high);
+  }
+
   /** BST.clone
    *    do recursive clone of tree 
    *    and update parentObj reference
@@ -47,9 +59,17 @@ class BST extends LinearCanvasObject {
   clone() {
     var copy = super.clone();
     copy.root = this.root.deepCopy();
-    for (var node of copy.inorder())
-      node.parentObject = copy;
+    copy.claimChildren(copy.root);
     return copy;
+  }
+
+  /** BST.claimChildren
+   *    update parentObject reference
+   *    in every node in given subtree (BSTNode object)
+   */
+  claimChildren(root) {
+    for (var node of root.inorder())
+      node.parentObject = this;
   }
 
   inorder() {
@@ -68,10 +88,26 @@ class BST extends LinearCanvasObject {
     return Array.from(this.inorder());
   }
 
+  /** BST.insert
+   *    recursive insert helper method
+   */
   insert(value) {
     this.root = this._insert(this.root, null, value);
   }
 
+  /** BST._insert
+   *    recursive bst insert 
+   *
+   *    new node gets instantiated with
+   *    references to CanvasState, this BST object,
+   *    the parent node (null if root) and a bst key (number)
+   *
+   *    thisBST.inserted is a field used when
+   *    undoing inserts to directly remove the 
+   *    most recently inserted node (the remove
+   *    method may not restore the previous shape 
+   *    of the tree)
+   */
   _insert(root, par,  value) {
     if (root == null) { 
       // maintain reference to last inserted node
@@ -85,6 +121,79 @@ class BST extends LinearCanvasObject {
       root.right = this._insert(root.right, root, value);
 
     return root;
+  }
+
+  /** BST.find
+   *    BST recursive find helper
+   */
+  find(value) {
+    return this._find(this.root, value);
+  }
+
+  /** BST._find
+   *    BST recursive find in O(logn)
+   */
+  _find(node, value) {
+    if (node.value == value) return node;
+    if (node.isLeaf()) return null;
+    if (value <= node.value) return this._find(node.leftChild(), value);
+    return this._find(node.rightChild(), value);
+  }
+
+  remove(value) {
+    if (this.find(value) == null) return;
+    if (this.root.isLeaf()) this.root = null;
+    else this.root = this._remove(this.root, value);
+  }
+
+  /** BST._remove
+   *   
+   *    TODO
+   *    fix case where node being removed has
+   *    two children and its predecessor has
+   *    the same value
+   */
+  _remove(node, value) {
+    if (node == null) return null;
+    if (node.value == value) {
+      if (node.leftChild() == null) return node.rightChild();
+      if (node.rightChild() == null) return node.leftChild();
+      
+      // swap current node with predecessor then remove
+      var predNode = node.pred();
+      this.swapNodes(predNode, node);      
+
+      // remove the node that was swapped to 
+      // predecessor's position
+      if (predNode === predNode.parNode.leftChild())
+        predNode.parNode.left = this._remove(predNode.parNode.leftChild(), value);
+      else predNode.parNode.right = this._remove(predNode.parNode.rightChild(), value);
+    }
+    else if (value < node.value)
+      node.left = this._remove(node.leftChild(), value);
+    else 
+      node.right = this._remove(node.rightChild(), value);
+
+    return node;
+  }
+
+  /** BST.swapNodes
+   *    swap nodes a and b by swapping
+   *    configurations -- much easier than
+   *    updating all the references
+   */
+  swapNodes(a, b) {
+    if (a === b) return;
+    if (a == null || b == null) throw "Cannot swap null nodes"
+
+    console.log("before swap!");
+    console.log("par of ", a.value, " = ", a.parNode ? a.parNode.value : null);
+    console.log("par of ", b.value, " = ", b.parNode ? b.parNode.value : null);
+    var tempNode = a.config();
+    Object.assign(a, b.config());
+    Object.assign(b, tempNode);
+    console.log("par of ", a.value, " = ", a.parNode ? a.parNode.value : null);
+    console.log("par of ", b.value, " = ", b.parNode ? b.parNode.value : null);
   }
 
   configureOptions() {
@@ -167,7 +276,6 @@ class BSTNode extends NodeObject {
       "bg": "fill",
       "background": "fill",
       "fill": "fill",
-      "value": "value",
       "showVal": "showValues",
       "fg": "textColor",
       "ind": "showIndices",
@@ -217,14 +325,74 @@ class BSTNode extends NodeObject {
     return this.right;
   }
 
+  /** BSTNode.getMax
+   *    return rightmost (maximum key)
+   *    node in this subtree
+   */
+  getMax() {
+    if (this.rightChild())
+      return this.rightChild().getMax();
+    return this;
+  }
+
+  /** BSTNode.getMin
+   *    return leftmost (minimum key)
+   *    node in this subtree
+   */
+  getMin() {
+    if (this.leftChild())
+      return this.leftChild().getMin();
+    return this;
+  }
+
+  /** BSTNode.pred
+   *    return inorder predecessor
+   */
+  pred() {
+    if (this.leftChild())
+      return this.leftChild().getMax();
+    var curNode = this; 
+    var parNode = this.parNode;
+    while (parNode && (curNode !== parNode.rightChild())) {
+      curNode = parNode;
+      parNode = curNode.parNode;
+    }
+    return curNode;
+  }
+
+  /** BSTNode.succ
+   *    return inorder successor 
+   */
+  succ() {
+    if (this.rightChild())
+      return this.rightChild().getMin();
+    var curNode = this; 
+    var parNode = this.parNode;
+    while (parNode && (curNode !== parNode.leftChild())) {
+      curNode = parNode;
+      parNode = curNode.parNode;
+    }
+    return curNode;
+  }
+
   isLeaf() {
     return this.left == null && this.right == null;
   }
 
-  deepCopy() {
+  /** BSTNode.deepCopy
+   *    perform deep copy of root node
+   *    for cloning. nodes still need
+   *    reference to parent BST 
+   *    (taken care of in BST.clone)
+   *
+   *    update parentNode references 
+   *    as part of traversal
+   */
+  deepCopy(par) {
     var copy = this.clone();
-    if (this.left) copy.left = this.left.deepCopy();
-    if (this.right) copy.right = this.right.deepCopy();
+    if (this.left) copy.left = this.left.deepCopy(copy);
+    if (this.right) copy.right = this.right.deepCopy(copy);
+    copy.parNode = par;
     return copy;
   }
 
@@ -267,7 +435,6 @@ class BSTNode extends NodeObject {
     this.ctx.fillStyle = this.textColor;
     this.ctx.fillText(valStr, this.x, this.y);
   }
-
 
   /** BSTNode.draw
    *    draw node with center at x, y and 
