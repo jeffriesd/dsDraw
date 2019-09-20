9569c22c1ef8b95aed79c638ba92272b207d6ebb
danjeffries96 <danjeffries96@github.com>
2019-09-20 15:28:18 -0400

    no longer use command objects where they're not necessary

--- public/static-toolbars.js
+++ public/static-toolbars.js
@@ -1,23 +1,42 @@
 (function initDOM() {
+
+  // if clip menu is locked return this alert
+  const cmLockedAlert = () => alert("Clip can not be changed currently.")
+
   // initialize clip menu buttons
   $("#contButton").click((event) => {
+    if (clipMenuLocked()) return cmLockedAlert()
     MediaController.getInstance().newClipFromCurrent();
   });
   $("#blankButton").click((event) => {
+    if (clipMenuLocked()) return cmLockedAlert()
     MediaController.getInstance().newClipBlank();
   });
   $("#deleteClipButton").click((event) => {
+    if (clipMenuLocked()) return cmLockedAlert()
     var clipIds = $(".activeClip")
       .toArray().map(x => parseInt(x.id.replace("thumbnail", "")));
-    var delCmd = new DeleteClipCommand(CanvasState.getInstance(), clipIds);
-    delCmd.execute();
+
+    var mc = MediaController.getInstance();
+    mc.removeClips(clipIds);
   });
 
   $("#exportClipButton").click((event) => {
+    if (clipMenuLocked()) return cmLockedAlert()
     var clipIds = $(".activeClip")
       .toArray().map(x => parseInt(x.id.replace("thumbnail", "")))
       .sort();
-    var exportCmd = new ExportVideoCommand(CanvasState.getInstance(), clipIds);
-    exportCmd.execute();
+
+    var mc = MediaController.getInstance();
+
+    var conn = ClientSocket.getInstance();
+
+    // lock mediacontroller actions
+    // video processing happens
+    mc.waiting = true;
+    
+    var body = { clipIds: clipIds };
+    conn.sendServer("merge", body);
+
   });
 })();
