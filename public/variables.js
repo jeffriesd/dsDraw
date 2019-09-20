0407161841127e050d9f4d343bac36b41caaf5df
danjeffries96 <danjeffries96@github.com>
2019-09-20 15:27:49 -0400

    keep track of aliases of canvas objects

--- public/variables.js
+++ public/variables.js
@@ -15,6 +15,11 @@ const keywords = {
   "array": null,
   "array1d": null, 
   "linked": null,
+  "udgraph": null,
+  "ugraph": null,
+  "digraph": null,
+  "dgraph": null,
+  "image": null,
   "tb": null,
   "tbox": null,
   "text": null,
@@ -39,6 +44,11 @@ class VariableEnvironment {
     this.functions = new Map();
     this.mainVariables = new Map();
     this.stack = [];
+
+
+    // keep track of alises
+    this.aliasOf = new Map(); // variable -> variable
+    this.aliases = new Map(); // variable -> Set(variable)
 
     VariableEnvironment.instance = this;
   }
@@ -78,6 +88,53 @@ class VariableEnvironment {
     stack.pop();
   }
 
+  static clone() {
+    return VariableEnvironment.getInstance().clone();
+  }
+
+  clone() {
+    return {
+      functions: new Map(this.functions),
+      mainVariables: new Map(this.mainVariables),
+      stack: this.stack.map(m => new Map(m)),
+      aliasOf: new Map(this.aliasOf),
+      aliases: new Map(this.aliases),
+    }
+  }
+
+  static setState(state) {
+    VariableEnvironment.getInstance().setState(state);
+  }
+
+  setState(state) {
+    this.function = new Map(state["functions"]);
+    this.mainVariables = new Map(state["mainVariables"]);
+    this.stack = state["stack"].map(m => new Map(m));
+    this.aliasOf = new Map(state["aliasOf"]);
+    this.aliases = new Map(state["aliases"]);
+
+    this.updateEnvironmentDisplay();
+  }
+
+  static clearAll() {
+    VariableEnvironment.getInstance().clearAll();
+  }
+
+  /** VariableEnvironment.clearAll  
+   *    clear all entries in current environment
+   */
+  clearAll() {
+    this.functions = new Map();
+    this.mainVariables = new Map();
+    this.stack = [];
+
+    // keep track of alises
+    this.aliasOf = new Map(); // variable -> variable
+    this.aliases = new Map(); // variable -> Set(variable)
+
+    this.updateEnvironmentDisplay();
+  }
+
   static setVar(varName, value) {
     return VariableEnvironment.getInstance().setVar(varName, value);
   }
@@ -96,8 +153,53 @@ class VariableEnvironment {
 
     if (this.hasVar(varName) && this.getVar(varName) instanceof FunctionDefinition)
       throw "Cannot reassign function name";
+
+    if (value instanceof CanvasObject && value.label != varName) {
+      // note that variableName is now an alias
+      // for the canvas object (this.value)'s label
+      this.addAlias(value.label, varName);
+    }
   
     this.variables.set(varName, value);
+
+    // update environment display
+    this.updateEnvironmentDisplay();
+  }
+
+  /** VariableEnvironment.addAlias
+   *    keep track of aliases of canvas objects
+   *    so they can be cleared when true label is cleared
+   */
+  addAlias(canvasObjectName, newName) {
+    if (canvasObjectName == "") return; // first assignment
+
+    // if canvasObjectName deleted, delete alias bindings
+    // as well 
+    if (! this.aliases.has(canvasObjectName)) this.aliases.set(canvasObjectName, new Set());
+    this.aliases.get(canvasObjectName).add(newName);
+
+    // newName is an alias of canvasObjectName, 
+    // so if newName is deleted, delete aliases also
+    this.aliasOf.set(newName, canvasObjectName);
+  }
+
+  getAllBindings() {
+    return new Map(Array.from(this.variables.entries())
+      .concat(Array.from(this.functions.entries())));
+  }
+
+
+  static updateEnvironmentDisplay() {
+    VariableEnvironment.getInstance().updateEnvironmentDisplay();
+  }
+
+  /** VariableEnvironment.updateEnvironmentDisplay
+   *    pass variable and function bindings over to react
+   *    component for display
+   */
+  updateEnvironmentDisplay() {
+    if (window.environmentPane == undefined) return;
+    window.environmentPane.setState({ variables: this.getAllBindings() });
   }
 
   static getVar(varName) {
@@ -122,6 +224,8 @@ class VariableEnvironment {
     if (this.functions.has(funcName) || this.variables.has(funcName))
       throw `Function definition error: ${funcName} already in use.`;
     this.functions.set(funcName, funcDef);
+
+    this.updateEnvironmentDisplay();
   }
 
   static deleteFunctionDefinition(funcName) {
@@ -132,6 +236,8 @@ class VariableEnvironment {
     if (! this.functions.has(funcName)) 
       throw `Cannot undo definition of unknown function ${funcName}.`;
     this.functions.delete(funcName);
+
+    this.updateEnvironmentDisplay();
   }
 
   static getCanvasObj(objLabel) {
@@ -147,14 +253,57 @@ class VariableEnvironment {
     return canvasObj;
   }
 
-  static deleteVar(varName) {
-    VariableEnvironment.getInstance().deleteVar(varName);
+  static deleteVar(varName, deleteAliases) {
+    return VariableEnvironment.getInstance().deleteVar(varName, deleteAliases);
   }
 
-  deleteVar(varName) {
+  /** VariableEnvironment.deleteVar
+   *  delete some binding(s) and return a map of 
+   *  what got deleted for undoing
+   */
+  deleteVar(varName, deleteAliases) {
     if (! this.hasVar(varName))
       throw `Cannot delete undefined variable '${varName}'.`;
-    this.variables.delete(varName);
+
+    // default to always deleting aliases
+    if (deleteAliases == undefined) 
+      deleteAliases = true;
+
+    //  keep track of what got deleted
+    var deleted = new Map();
+
+    const deleteBinding = k => {
+      deleted.set(k, this.variables.get(k));
+      this.variables.delete(k);
+    }
+
+    if (! deleteAliases) 
+      deleteBinding(varName);
+    else {
+
+      // varName is an alias of a canvas object
+      // with another name
+      if (this.aliasOf.has(varName)) {
+        var trueLabel = this.aliasOf.get(varName);
+        var otherNames = this.aliases.get(trueLabel);
+        otherNames.forEach(o => deleteBinding(o));
+        deleteBinding(trueLabel);
+      } 
+      else {
+        // varName is not an alias but it 
+        // has aliases
+        if (this.aliases.has(varName)) {
+          // delete aliases if they exist
+          this.aliases.get(varName).forEach(other => deleteBinding(other));
+        }
+        deleteBinding(varName);
+      }
+    }
+
+    // update environment display
+    this.updateEnvironmentDisplay();
+
+    return deleted;
   }
 
   static hasVar(varName) {
