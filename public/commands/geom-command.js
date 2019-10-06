c230473b85ac2969ab85c03b4f8ed6b931a5db24
danjeffries96 <danjeffries96@github.com>
2019-10-06 18:25:03 -0400

    add moveTo command

--- public/commands/geom-command.js
+++ public/commands/geom-command.js
@@ -24,7 +24,7 @@ class TranslateCommand extends GeomCommand {
   getChildValues() {
     this.receiver = this.args[0];
     this.dx = this.args[1];
-    this.dy = this.args[1];
+    this.dy = this.args[2];
   }
 
   checkArguments() {
@@ -37,6 +37,51 @@ class TranslateCommand extends GeomCommand {
 
   executeSelf() {
     this.receiver.move(this.dx, this.dy);
+  }
+
+  saveState() {
+    return { 
+      x : this.receiver.x, 
+      y : this.receiver.y,
+    };
+  }
+
+  restoreState(state) {
+    this.receiver.moveTo(state.x, state.y);
+  }
+}
+
+
+/** same as translate but absolute instead of relative */
+class MoveToCommand extends GeomCommand {
+  constructor(cState, ...args) {
+    super(...args);
+  }
+
+  usage() {
+    return "moveTo(canvasObj, dx, dy)";
+  }
+
+  precheckArguments() {
+    this.checkArgsLength(3);
+  }
+
+  getChildValues() {
+    this.receiver = this.args[0];
+    this.dx = this.args[1];
+    this.dy = this.args[2];
+  }
+
+  checkArguments() {
+    if (! (this.receiver instanceof CanvasObject))
+      throw "Only canvas objects can be translated";
+    if (! (typeof this.dx) === "number" 
+        && (typeof this.dy) === "number")
+      this.argsError("Please user integer dx, dy")
+  }
+
+  executeSelf() {
+    this.receiver.moveTo(this.dx, this.dy);
   }
 
   saveState() {
