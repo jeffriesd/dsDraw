9071de6dc56ea9dfaf1ef7c48d14e4c641fba825
danjeffries96 <danjeffries96@github.com>
2019-10-06 18:25:53 -0400

    implement list[] commands (previoulsy linked list commands were in here)

--- public/commands/list-command.js
+++ public/commands/list-command.js
@@ -1,135 +1,51 @@
-/** LinkedList commands
- */
 
-class LinkedListCommand extends CanvasObjectMethod {
-  checkIndices(...indices) {
-    indices.forEach(i => {
-      if (! (this.receiver.list.hasEquiv(i))) {
-        this.argsError(`Invalid index: ${i}.`);
-      }
-    });
+class ListCommand extends ConsoleCommand {
+  constructor(receiver, ...args) {
+    super(...args);
+    this.receiver = receiver;
   }
 
   saveState() {
     return {
-      previousList : new Map(this.receiver.list),
-      previousArrows : new Map(this.receiver.arrows),
+      prevList : this.receiver.slice()
     }
   }
 
+  // replace array contents in place
   restoreState(state) {
-    this.receiver.list = new Map(state.previousList);
-    this.receiver.arrows = new Map(state.previousArrows);
+    this.receiver.splice(0, this.receiver.length, ...state.prevList);
   }
 }
 
-class LinkedListInsertCommand extends LinkedListCommand {
-
-  getChildValues() {
-     
-    this.fromIndex = this.args[0];
-    this.value = this.args[1];
-  }
-
-  checkArguments() {
-    this.checkIndices(this.fromIndex);
-    if (isNaN(Number(this.value)))
-      this.argsError("Invalid value for linked list:" + this.value);
-  }
-
+class ListLengthCommand extends ListCommand { 
   executeSelf() {
-    this.fromNode = this.receiver.list.get(this.fromIndex);
-    // addNode creates new node and new edge
-    this.newNode = this.receiver.addNode(this.fromNode, this.value);
+    return this.receiver.length;
   }
 
-  // undoSelf() {
-  //   // remove node and newly created edge
-  //   this.receiver.removeNode(this.newNode);
-  // }
-
+  saveState() {}
+  restoreState() {}
 }
 
-class LinkedListLinkCommand extends LinkedListCommand {
-
+class ListPushCommand extends ListCommand {
   getChildValues() {
-     
-    this.fromIndex = this.args[0];
-    this.toIndex = this.args[1];
-  }
-
-  checkArguments() {
-    this.checkIndices(this.fromIndex, this.toIndex);
+    this.element = this.args[0];
   }
 
   executeSelf() {
-    if (this.newState == undefined) {
-    this.fromNode = this.receiver.list.get(this.fromIndex);
-    this.toNode = this.receiver.list.get(this.toIndex);
-    this.receiver.addEdge(this.fromNode, this.toNode);
-    this.newState = this.saveState();
-    }
-    else this.restoreState(this.newState);
+    this.receiver.push(this.element);
   }
-
-  // undoSelf() {
-  //   this.receiver.removeEdge(this.fromNode, this.toNode);
-  // }
 }
 
-class LinkedListCutCommand extends LinkedListCommand {
 
-  getChildValues() {
-     
-    this.fromIndex = this.args[0];
-    this.toIndex = this.args[1];
-  }
 
-  checkArguments() {
-    this.checkIndices(this.fromIndex, this.toIndex);
-  }
-
+class ListPopCommand extends ListCommand {
   executeSelf() {
-    this.fromNode = this.receiver.list.get(this.fromIndex);
-    this.toNode = this.receiver.list.get(this.toIndex);
-    this.edge = this.receiver.removeEdge(this.fromNode, this.toNode);
+    return this.receiver.pop();
   }
-
-  /** LinkedListCutCommand.undo 
-   *    restore cut edge in arrows map
-   */
-  // undoSelf() {
-  //   this.receiver.arrows.set([this.fromIndex, this.toIndex], this.edge);
-  // }
 }
 
-class LinkedListRemoveCommand extends LinkedListCommand {
-  constructor(receiver, removeIdx) {
-    super(receiver, removeIdx);
-    this.node = null;
-  }
-
-  getChildValues() {
-     
-    this.removeIndex = this.args[0];
-  }
-
-  checkArguments() {
-    this.checkIndices(this.removeIndex);
-  }
-
+class ListEmptyCommand extends ListCommand {
   executeSelf() {
-    // if (this.node == null) {
-    //   this.node = this.receiver.list.get(this.removeIndex);
-    // }
-    if (this.newState == undefined) {
-      this.receiver.list.delete(this.removeIndex);
-      this.newState = this.saveState();
-    }
-    else this.restoreState(this.newState);
+    return this.receiver.length == 0;
   }
-
-  // undoSelf() {
-  //   this.receiver.list.set(this.removeIndex, this.node);
-  // }
 }
