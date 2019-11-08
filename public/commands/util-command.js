c479a355e37d4bd7b727a05973c64b5b7fdcb812
danjeffries96 <danjeffries96@github.com>
2019-11-07 22:20:28 -0500

    remove deprecated truncate command

--- public/commands/util-command.js
+++ public/commands/util-command.js
@@ -39,24 +39,6 @@ class VideoCommand extends UtilCommand {
   }
 }
 
-class TruncateVideoCommand extends VideoCommand {
-  // truncate video file as well as command recorder state
-  execute() {
-    if (this.mc.getState() !== this.mc.pauseState) return;
-    var conn = ClientSocket.getInstance();
-    var currTime = this.mc.player.video.currentTime;
-
-    var clipId = parseInt(this.mc.activeClipId);
-
-    this.mc.waiting = true;
-    this.mc.cmdRecorder.truncate(currTime);
-
-    // send message from client to server to truncate video at current time
-    var body = { clipId : clipId, timeStamp: currTime };
-    conn.sendServer("truncate", body);
-  }
-}
-
 class RepaintCommand extends UtilCommand {
   constructor(cState) {
     super();
